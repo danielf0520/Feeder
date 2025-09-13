0ba045f9cea0126a71606d236ed47b962a00c181
danielfeng <danielf6635@nycstudents.net>
2025-09-13 18:22:13 -0400

    final feeder code (10,5) simulated days

--- Main.java
+++ Main.java
@@ -2,15 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(500);
-        System.out.println(f);
-        System.out.println("current food: " + f.getCurrentFood());
-        f.simulateOneDay(12);
-        System.out.println("current food: " + f.getCurrentFood());
-        f= new Feeder(1000);
-        f.simulateOneDay(22);
-        System.out.println("current food: " + f.getCurrentFood());
-        f= new Feeder(2400);
-        System.out.println(f.simulateManyDays());
+        Feeder f = new Feeder(250);
+        System.out.println(f.simulateManyDays(10,5) + " day(s)");
+        f = new Feeder(0);
+        System.out.println(f.simulateManyDays(5,10) + " day(s)");
     }
 }
